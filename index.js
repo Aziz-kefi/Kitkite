1ed7080335fa38c884e2b321a4a143dc1fbca8ee
Aziz Kéfi <58823874+Aziz-kefi@users.noreply.github.com>
2022-11-13 20:38:32 +0100

    first back , still bugs

---
+++ index.js
@@ -0,0 +1,69 @@
+require("dotenv").config();
+const swaggerUI= require("swagger-ui-express") 
+const swaggerJsDoc= require("swagger-jsdoc")
+// DEPENDENCIES
+const express = require("express");
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const morgan = require("morgan")
+
+// VARIABLES
+const app = express();
+const port = process.env.PORT || 5000
+
+// MIDDLEWARES
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static('uploads/images'))
+app.use('/images-files', express.static('uploads/images'))
+
+// DATABASE
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(
+    () => {
+      console.log("Database connected")
+    },
+    (err) => {
+      console.log("Error connecting to the database", err)
+    }
+  )
+  
+/////SWAGGER
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+      termsOfService: "http://example.com/terms/",
+      contact: {
+        name: "API Support",
+        url: "http://www.exmaple.com/support",
+        email: "support@example.com",
+      },
+    },
+
+    servers: [
+      {
+        url: "http://localhost:5000/user",
+        description: "My API Documentation",
+      },
+    ],
+  },
+  apis: ["./routes/user-route.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
+// ROUTES
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+app.use("/user", require("./routes/user-route"))
+
+// SERVER START
+const server = app.listen(port, () => {
+  const port = server.address().port;
+  console.log(`Server running on port ${port}`);
+});
---
+++ routes/user-route.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const router = express.Router()
+const userController = require("../controllers/user-controller");
+const upload = require('../middlewares/storage-images');
+
+
+/**
+ * @swagger
+ * /:
+ *   get: 
+ *      summary: function get all 
+ *      description: ok
+ *      responses:
+ *              200:
+ *              description: to test methods
+ */
+router.get("/",userController.getAll)
+
+router.post("/register", upload.single('picture'), userController.register);
+router.post("/login", userController.login);
+router.post("/login-with-social", userController.loginWithSocial);
+router.post("/send-confirmation-email", userController.sendConfirmationEmail);
+router.get("/confirmation/:token", userController.confirmation);
+router.post("/forgot-password", userController.forgotPassword);
+router.put("/update-profile", upload.single('picture'), userController.updateProfile);
+router.put("/update-password", userController.updatePassword);
+
+router.route("/one")
+    .post(userController.get)
+    .delete(userController.delete);
+
+router.route("/all").get(userController.getAll).delete(userController.deleteAll);
+
+module.exports = router
